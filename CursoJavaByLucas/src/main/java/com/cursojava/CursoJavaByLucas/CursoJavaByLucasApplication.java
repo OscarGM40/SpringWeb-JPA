b6cd291e16398ef7a12f9c9e85c08e5c30a5f5ed
Oscar <oscargm28@hotmail.com>
2022-01-02 19:04:03 +0100

    initialized Great course

---
+++ CursoJavaByLucas/src/main/java/com/cursojava/CursoJavaByLucas/CursoJavaByLucasApplication.java
@@ -0,0 +1,13 @@
+package com.cursojava.CursoJavaByLucas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoJavaByLucasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoJavaByLucasApplication.class, args);
+	}
+
+}
---
+++ CursoJavaByLucas/src/test/java/com/cursojava/CursoJavaByLucas/CursoJavaByLucasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursojava.CursoJavaByLucas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoJavaByLucasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
