4b2f1b278347158f1700be260aa7eff649e9022b
Oscar <oscargm28@hotmail.com>
2022-02-19 22:22:28 +0100

    sending email

--- CompleteRegistrationSystemWithJavaEmailSending/src/main/java/com/example/demo/controller/RegistrationController.java
+++ CompleteRegistrationSystemWithJavaEmailSending/src/main/java/com/example/demo/controller/RegistrationController.java
@@ -22,7 +22,7 @@ import lombok.extern.slf4j.Slf4j;
 public class RegistrationController {
 
   @Autowired
-  private RegistrationService registrationService;
+  private final RegistrationService registrationService;
  
   @PostMapping
   public String register(@RequestBody RegistrationRequest request) {
